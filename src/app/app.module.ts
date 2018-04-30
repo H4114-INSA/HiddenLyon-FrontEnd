bbe8de686b51c09edac9e599858c2222b11e81df
Ros <rosbenjamindu01@gmail.com>
2018-04-30 11:04:31 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,9 @@ import {AddPointInfosPage} from "../pages/addPointInfos/addPointInfos";
 
 import { ParametresPage} from "../pages/parametres/parametres";
 import {ConditionsPage} from "../pages/conditions/conditions";
+import {AddPointPage} from "../pages/addPoint/addPoint";
+import {AddPointInfosPage} from "../pages/addPointInfos/addPointInfos";
+
 
 import { Geolocation } from '@ionic-native/geolocation';
 import { StatusBar } from '@ionic-native/status-bar';
